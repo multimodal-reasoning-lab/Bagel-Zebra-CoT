1a6658333b9a3decca7409683d1b1a7c21a1a8e9
Kaiyu Yue <kaiyuyue@meta.com>
2025-08-05 12:32:22 -0700

    fix path

--- download_model.py
+++ download_model.py
@@ -1,16 +1,12 @@
 from huggingface_hub import snapshot_download
 
-HF_HOME = "path/to/your/hf_home"
-
-# Download Bagel-Zebra-CoT
-save_dir = HF_HOME + "models/Bagel-Zebra-CoT"
+HF_HOME = "/mnt/wsfuse/kaiyuyue/cache/huggingface"
 repo_id = "multimodal-reasoning-lab/Bagel-Zebra-CoT"
-cache_dir = HF_HOME + save_dir + "/cache"
 
-snapshot_download(cache_dir=cache_dir,
-  local_dir=save_dir,
-  repo_id=repo_id,
-  local_dir_use_symlinks=False,
-  resume_download=True,
-  allow_patterns=["*.json", "*.safetensors", "*.bin", "*.py", "*.md", "*.txt"],
+snapshot_download(
+    cache_dir=HF_HOME,
+    repo_id=repo_id,
+    local_dir_use_symlinks=False,
+    resume_download=True,
+    allow_patterns=["*.json", "*.safetensors", "*.bin", "*.py", "*.md", "*.txt"],
 )
